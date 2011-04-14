a24c7852f5a54355470ebb130be7ce1265894c21
Ben Hoskings <ben@hoskings.net>
2011-04-14 16:58:57 +1000

    Whitespace.

--- backups.rb
+++ backups.rb
@@ -1,21 +1,10 @@
 dep 'db backup exists' do
   @backup_time = Time.now
 
-  def backup_prefix
-    "~/sqldumps".p
-  end
-
-  def refspec
-    shell "git rev-parse --short HEAD"
-  end
-
-  def sqldump
-    backup_prefix / "tc_production-#{refspec}-#{@backup_time.strftime("%Y-%m-%d-%H:%M:%S")}.psql"
-  end
-
-  def backup_path
-    "#{sqldump}.gz".p
-  end
+  def backup_prefix; "~/sqldumps".p end
+  def refspec; shell "git rev-parse --short HEAD" end
+  def sqldump; backup_prefix / "tc_production-#{refspec}-#{@backup_time.strftime("%Y-%m-%d-%H:%M:%S")}.psql" end
+  def backup_path; "#{sqldump}.gz".p end
 
   met? { backup_path.exists? }
   before { backup_prefix.mkdir }
