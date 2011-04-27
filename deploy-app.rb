dfd2ff26cffa28f9b6503b012ad5eebb87eda421
Michael Morris <mtcmorris@gmail.com>
2011-04-27 11:14:12 +1000

    Maintenance page now up only prior to migrations (not offsite backup)

--- deploy-app.rb
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-
-dep 'app deployed' do
-  requires 'offsite backup.cloudfiles', '☕ & db'
-end
-
-dep '☕ & db', :template => 'benhoskings:task' do
-  run { bundle_rake 'barista:brew db:migrate db:autoupgrade data:migrate tc:data:production' }
-end
