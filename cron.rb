2ec05c935a04a57246c5a7ca4cbf0a4514d5c0f7
Ben Hoskings <ben@hoskings.net>
2011-04-28 13:54:23 +1000

    Replaced 'cron jobs' /etc/cron.* linker with 'crontab' meta, to write a user-specific crontab.

---
+++ cron.rb
@@ -0,0 +1,53 @@
+dep 'crontab' do
+  setup {
+    @hour = 11 # 1am in GMT+10
+    @min = rand(60)
+  }
+  met? {
+    babushka_config? "/etc/crontab"
+  }
+  meet {
+    render_erb "utils/crontab.erb", :to => "/etc/crontab", :sudo => true
+  }
+end
+
+meta :cronjob do
+  accepts_value_for :timing
+  accepts_value_for :command
+  def entry
+    "#{timing} #{command}"
+  end
+  template {
+    met? {
+      output = failable_shell('crontab -l').stdout
+      if output.include?(entry)
+        true
+      elsif output.split("\n").detect {|l| l.ends_with?(command) }
+        met "The job is scheduled, but with different timing."
+      end
+    }
+    meet {
+      shell 'crontab -', input: [shell('crontab -l'), entry].compact.join("\n").end_with("\n")
+    }
+  }
+end
+
+dep 'cronjobs' do
+  requires 'hourly.cronjob', 'daily.cronjob', 'weekly.cronjob'
+end
+
+dep 'hourly.cronjob' do
+  timing '18 * * * *'
+  command 'cd ~/current && ./script/tasks/hourly >> log/tasks.log'
+end
+
+dep 'daily.cronjob' do
+  # hour 11 is 1am in GMT+10.
+  timing '33 11 * * *'
+  command 'cd ~/current && ./script/tasks/daily >> log/tasks.log'
+end
+
+dep 'weekly.cronjob' do
+  timing '48 11 * * 7'
+  command 'cd ~/current && ./script/tasks/weekly >> log/tasks.log'
+end
--- utils.rb
+++ utils.rb
@@ -1,33 +1,3 @@
-dep 'crontab' do
-  setup {
-    @hour = 11 # 1am in GMT+10
-    @min = rand(60)
-  }
-  met? {
-    babushka_config? "/etc/crontab"
-  }
-  meet {
-    render_erb "utils/crontab.erb", :to => "/etc/crontab", :sudo => true
-  }
-end
-
-dep 'cron jobs' do
-  def job_path job_name
-    "/etc/cron.#{job_name}/tc_#{job_name}".p
-  end
-  def missing_jobs
-    %w[hourly daily weekly].reject {|job|
-      job_path(job).exists?
-    }
-  end
-  met? { missing_jobs.empty? }
-  meet {
-    missing_jobs.each {|job|
-      sudo "ln -sf '#{var(:rails_root)/'script/tasks'/job}' '#{job_path(job)}'"
-    }
-  }
-end
-
 dep 'ssl cert in place', :template => 'benhoskings:nginx' do
   def names
     %w[key crt].map {|ext| "#{var(:domain)}.#{ext}" }
