286100dd6a2584c8bbd12389bb459a1bee1f6248
Ben Hoskings <ben@hoskings.net>
2011-05-06 10:25:17 +1000

    Added draft delayed_job deps.

---
+++ delayed_job.rb
@@ -0,0 +1,25 @@
+dep 'delayed job' do
+  requires 'delayed_job.supervisor'
+end
+
+dep 'delayed_job.supervisor' do
+  restart :always
+  command "bundle exec rake jobs:work RAILS_ENV=production"
+  user "theconversation.edu.au"
+  directory "/srv/http/#{user}/current"
+  met? {
+    !shell("ps aux").split("\n").grep(/rake jobs:work$/).empty?
+  }
+end
+
+dep 'delayed job restarted.task' do
+  run {
+    output = shell('ps aux | grep "rake jobs:work" | grep -v grep')
+
+    if output.nil?
+      log_error "`rake jobs:work` isn't running."
+    else
+      shell "kill -s TERM #{@output.scan(/^\w+\s+(\d+)\s+/).flatten.first}"
+    end
+  }
+end
