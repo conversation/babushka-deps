6ec80acfef2d017fe0e88eda3906acd85d3d89b5
Ben Hoskings <ben@hoskings.net>
2011-10-03 12:00:10 +1100

    Re-added packages deps, since they have to be run before the app code is pushed.

---
+++ provisioning.rb
@@ -0,0 +1,17 @@
+dep 'theconversation.edu.au packages' do
+  requires [
+    'libxml.managed', # for nokogiri
+    'libxslt.managed',  # for nokogiri
+    'imagemagick.managed', # for paperclip
+    'coffeescript.src', # for barista
+    'supervisor.managed'
+  ]
+end
+
+dep 'jobs.theconversation.edu.au packages' do
+  requires [
+    'imagemagick.managed', # for paperclip
+    'postgresql-contrib-9.0.managed', # for search
+    'tidy.managed' # for upmark
+  ]
+end
