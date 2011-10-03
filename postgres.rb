9fa7ffc58f4f21a200e8d44bfd2485a1f42364ce
Ben Hoskings <ben@hoskings.net>
2011-10-03 14:09:10 +1100

    Added 'postgres extension installed', to install the trigram extensions.

--- postgres.rb
+++ postgres.rb
@@ -3,3 +3,18 @@ dep 'read-only db permissions' do
   met? { shell "psql #{var(:db_name)} -c 'SELECT id FROM messages LIMIT 1'" }
   meet { sudo %Q{psql #{var(:db_name)} -c 'GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO "#{var(:username)}"'}, as: 'postgres' }
 end
+
+dep 'postgres extension installed', :proc_name, :extension do
+  met? {
+    shell("psql postgres -t",
+      as: 'postgres',
+      input: "select count(*) from pg_proc where proname = '#{proc_name}'"
+    ).strip.to_i > 0
+  }
+  meet {
+    shell("psql postgres",
+      as: 'postgres',
+      input: (shell('pg_config --sharedir') / 'contrib' / extension).p.read
+    )
+  }
+end
