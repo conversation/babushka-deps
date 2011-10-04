994b5c09a8f1676c29dc0037fe280a2bf516e396
Ben Hoskings <ben@hoskings.net>
2011-10-04 16:49:36 +1100

    OK, that was all wrong. Properly updated 'postgres extension' to use 9.1's new extension installing.

--- postgres.rb
+++ postgres.rb
@@ -4,19 +4,17 @@ dep 'read-only db permissions' do
   meet { sudo %Q{psql #{var(:db_name)} -c 'GRANT SELECT ON ALL TABLES IN SCHEMA "public" TO "#{var(:username)}"'}, as: 'postgres' }
 end
 
-dep 'postgres extension installed', :username, :db_name, :proc_name, :extension do
+dep 'postgres extension', :username, :db_name, :extension do
   requires 'benhoskings:existing postgres db'.with(username, db_name)
 
-  met? {
-    shell("psql #{db_name} -t",
-      as: 'postgres',
-      input: "SELECT count(*) FROM pg_proc WHERE proname = '#{proc_name}'"
-    ).strip.to_i > 0
-  }
+  def psql cmd
+    shell("psql #{db_name} -t", as: 'postgres', input: cmd).strip
+  end
 
+  met? {
+    psql("SELECT count(*) FROM pg_extension WHERE extname = '#{extension}'").to_i > 0
+  }
   meet {
-    shell "psql #{db_name}",
-      as: 'postgres',
-      input: (shell('pg_config --sharedir') / extension).p.read
+    psql("CREATE EXTENSION '#{extension}'")
   }
 end
--- provisioning.rb
+++ provisioning.rb
@@ -20,7 +20,7 @@ dep 'jobs.theconversation.edu.au provisioned', :username, :db_name do
   requires [
     'jobs.theconversation.edu.au packages',
     'cronjobs',
-    'postgres extension installed'.with(username, db_name, 'similarity', 'extension/pg_trgm--1.0.sql')
+    'postgres extension'.with(username, db_name, 'pg_trgm')
   ]
 end
 
