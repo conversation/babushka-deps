8da9949c333ec060d9dea26390aac91d18d4f90e
Ben Hoskings <ben@hoskings.net>
2011-10-05 13:53:05 +1100

    Made backup_path a param, instead of a method looked up across deps.

--- backups.rb
+++ backups.rb
@@ -8,13 +8,14 @@ dep 'db backed up', :env do
   }
 end
 
-dep 'db backup exists' do
+dep 'db backup exists', :backup_path do
   @backup_time = Time.now
 
   def backup_prefix; "~/sqldumps".p end
   def refspec; shell "git rev-parse --short HEAD" end
   def sqldump; backup_prefix / "tc_production-#{@backup_time.strftime("%Y-%m-%d-%H:%M:%S")}-#{refspec}.psql" end
-  def backup_path; "#{sqldump}.gz".p end
+
+  backup_path.default!("#{sqldump}.gz")
 
   met? { backup_path.exists? }
   before { backup_prefix.mkdir }
@@ -22,25 +23,20 @@ dep 'db backup exists' do
   after { log_shell "Removing old sqldumps", %Q{ls -t -1 #{backup_prefix} | tail -n+6 | while read f; do rm "#{backup_prefix}/$f"; done} }
 end
 
-dep 'offsite backup.cloudfiles' do
-  def backup_path
-    # TODO: make this better
-    Dep('db backup exists', from: dependency.dep_source).context.backup_path
-  end
-
-  def md5 backup_path
+dep 'offsite backup.cloudfiles', :backup_path do
+  def md5
     cmd = which('md5') ? 'md5 -q' : 'md5sum'
     shell("#{cmd} '#{backup_path}'").split(/\s/, 2).first
   end
 
-  requires 'db backup exists'
+  requires 'db backup exists'.with(backup_path)
 
   met? {
     upload_info = get_upload_info
     log "Rackspace returned HTTP #{upload_info.code}.", as: (:ok if upload_info.is_a?(Net::HTTPSuccess))
 
     if upload_info.is_a?(Net::HTTPSuccess)
-      local_hash = md5(backup_path)
+      local_hash = md5
       remote_hash = upload_info.header.to_hash['etag'].first
       if local_hash != remote_hash
         unmet "Checksum mismatch: local was #{local_hash} but rackspace reported #{remote_hash}."
