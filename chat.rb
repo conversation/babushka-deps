a6162f8a56544ac43721e1455c28eb890c44e006
Ben Hoskings <ben@hoskings.net>
2011-10-18 18:17:20 +1100

    Converted chat deps to use params.

--- chat.rb
+++ chat.rb
@@ -1,14 +1,15 @@
-dep 'chat' do
-  requires 'chat.supervisor'
+dep 'chat', :username do
+  requires 'chat.supervisor'.with(username: username)
 end
 
-dep 'chat.supervisor' do
-  requires 'chat app'
+dep 'chat.supervisor', :username, :db_name, :tc_path do
+  requires 'chat app'.with(username, db_name, tc_path)
 
-  set :chat_db, 'tc_production'
+  db_name.default!('tc_production')
+  tc_path.default!('/srv/http/theconversation.edu.au/current')
 
   command "node chat_server.js"
-  environment %Q{NODE_PATH="/usr/local/lib/node_modules"}, %Q{CHAT_DB="#{var(:chat_db)}"}
+  environment %Q{NODE_PATH="/usr/local/lib/node_modules"}, %Q{CHAT_DB="#{db_name}"}
   user "chat.theconversation.edu.au"
   directory "/srv/http/#{user}/current"
 
@@ -17,44 +18,45 @@ dep 'chat.supervisor' do
   }
 end
 
-dep 'chat app' do
+dep 'chat app', :username, :db_name, :tc_path do
   requires [
-    'chat db permissions',
-    'chat app symlinked in',
+    'chat db permissions'.with(username, db_name),
+    'chat app symlinked in'.with(tc_path),
     'socket.io.npm',
     'pg.npm'
   ]
 end
 
-dep 'chat db permissions' do
-  requires 'messages access', 'messages_id_seq access'
+dep 'chat db permissions', :username, :db_name do
+  requires 'messages access'.with(username, db_name)
+  requires 'messages_id_seq access'.with(username, db_name)
 end
 
-dep 'messages access' do
+dep 'messages access', :username, :db_name do
   requires 'benhoskings:postgres access'
-  met? { shell "psql #{var(:chat_db)} -c 'SELECT id FROM messages LIMIT 1'" }
-  meet { sudo %Q{psql #{var(:chat_db)} -c 'GRANT SELECT,INSERT ON messages TO "#{var(:username)}"'}, as: 'postgres' }
+  met? { shell "psql #{db_name} -c 'SELECT id FROM messages LIMIT 1'" }
+  meet { sudo %Q{psql #{db_name} -c 'GRANT SELECT,INSERT ON messages TO "#{username}"'}, as: 'postgres' }
 end
 
-dep 'messages_id_seq access' do
+dep 'messages_id_seq access', :username, :db_name do
   requires 'benhoskings:postgres access'
-  met? { shell "psql #{var(:chat_db)} -c 'SELECT sequence_name FROM messages_id_seq LIMIT 1'" }
-  meet { sudo %Q{psql #{var(:chat_db)} -c 'GRANT SELECT,UPDATE ON messages_id_seq TO "#{var(:username)}"'}, as: 'postgres' }
+  met? { shell "psql #{db_name} -c 'SELECT sequence_name FROM messages_id_seq LIMIT 1'" }
+  meet { sudo %Q{psql #{db_name} -c 'GRANT SELECT,UPDATE ON messages_id_seq TO "#{username}"'}, as: 'postgres' }
 end
 
-dep 'chat app symlinked in' do
+dep 'chat app symlinked in', :tc_path do
   def path
     'public/javascripts/chat_server.js'
   end
-  set :rails_root, "/srv/http/theconversation.edu.au/current"
+
   setup {
-    (var(:rails_root)/path).exists?.tap {|result|
-      log_error "#{var(:rails_root)/path} doesn't exist - it will be rendered by barista on deploy." unless result
+    (tc_path/path).exists?.tap {|result|
+      log_error "#{tc_path/path} doesn't exist - it will be rendered by barista on deploy." unless result
     }
   }
   met? { "~/current/chat_server.js".p.exists? }
   before { "~/current".p.mkdir }
-  meet { shell "ln -sf #{var(:rails_root)}/#{path} ~/current" }
+  meet { shell "ln -sf #{tc_path}/#{path} ~/current" }
 end
 
 dep 'socket.io.npm' do
