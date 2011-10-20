e9fbb632efa2c66142d77fe80e456eaaa9d40bbc
Ben Hoskings <ben@hoskings.net>
2011-10-21 10:56:39 +1100

    Extracted dev requirements from 'packages' deps into 'dev' deps.

--- provisioning.rb
+++ provisioning.rb
@@ -8,13 +8,20 @@ end
 
 dep 'theconversation.edu.au packages' do
   requires [
+    'theconversation.edu.au dev',
+    'supervisor.managed'
+  ]
+end
+
+dep 'theconversation.edu.au dev' do
+  requires [
     'libxml.managed', # for nokogiri
     'libxslt.managed', # for nokogiri
     'imagemagick.managed', # for paperclip
     'coffeescript.src', # for barista
-    'supervisor.managed'
   ]
 end
+
 
 dep 'jobs.theconversation.edu.au provisioned', :username, :db_name, :env do
   requires [
@@ -26,11 +33,12 @@ dep 'jobs.theconversation.edu.au provisioned', :username, :db_name, :env do
 end
 
 dep 'jobs.theconversation.edu.au packages' do
+  requires 'jobs.theconversation.edu.au dev'
+end
+
+dep 'jobs.theconversation.edu.au dev' do
   requires [
-    'libxml.managed', # for nokogiri
-    'libxslt.managed', # for nokogiri
-    'imagemagick.managed', # for paperclip
-    'coffeescript.src', # for barista
+    'theconversation.edu.au dev', # The same packages the main app uses
     'postgresql-contrib.managed', # for search
     'tidy.managed' # for upmark
   ]
