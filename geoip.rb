75514e02e4a3ebbb48d2143f99cbd4bd6fcd2e1b
Ben Hoskings <ben@hoskings.net>
2011-12-06 13:48:33 +1100

    Add 'geoip database' to 'theconversation.edu.au dev', passing in the root properly.

--- geoip.rb
+++ geoip.rb
@@ -6,10 +6,10 @@ dep 'geoip.managed' do
   provides 'geoiplookup', 'geoiplookup6', 'geoipupdate'
 end
 
-dep 'geoip database', :source, :app_root do
+dep 'geoip database', :source, :root do
   source.default!('http://www.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz')
   def local_path
-    app_root / 'db' / File.basename(source.to_s.chomp('.gz'))
+    root / 'db' / File.basename(source.to_s.chomp('.gz'))
   end
   met? {
     local_path.p.exists?
--- provisioning.rb
+++ provisioning.rb
@@ -1,24 +1,27 @@
-dep 'theconversation.edu.au provisioned', :env do
+dep 'theconversation.edu.au provisioned', :env, :root do
   requires [
-    'theconversation.edu.au packages',
+    'theconversation.edu.au packages'.with(root),
     'cronjobs'.with(env),
     'delayed job'.with(env)
   ]
 end
 
-dep 'theconversation.edu.au packages' do
+dep 'theconversation.edu.au packages', :root do
   requires [
-    'theconversation.edu.au dev',
+    'theconversation.edu.au dev'.with(root),
     'supervisor.managed'
   ]
 end
 
-dep 'theconversation.edu.au dev' do
+dep 'theconversation.edu.au dev', :root do
+  root.default('.')
   requires [
     'libxml.managed', # for nokogiri
     'libxslt.managed', # for nokogiri
     'imagemagick.managed', # for paperclip
     'coffeescript.src', # for barista
+
+    'geoip database'.with(root: root)
   ]
 end
 
