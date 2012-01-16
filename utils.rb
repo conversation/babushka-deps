974a8abc824442362d8511113e46a530dde3e4a9
Ben Hoskings <ben@hoskings.net>
2012-01-17 10:45:45 +1100

    Revert "Stay classy." 916c4f6 was a huge fail.
    This reverts commit 916c4f63458b897a2ecd775b49295945544683c0.
    
    Conflicts:
    
    	utils.rb

--- utils.rb
+++ utils.rb
@@ -1,6 +1,6 @@
-dep 'ssl cert in place', :nginx_prefix, :domain, :cert_path, :template => 'benhoskings:nginx' do
+dep 'ssl cert in place', :nginx_prefix, :domain, :cert_source, :template => 'benhoskings:nginx' do
   nginx_prefix.default('/opt/nginx')
-  cert_path.default('~/current/config/dollhouse/assets/certs')
+  cert_source.default('~/current/config/dollhouse/assets/certs')
   def names
     %w[key crt].map {|ext| "#{domain}.#{ext}" }
   end
@@ -11,6 +11,6 @@ dep 'ssl cert in place', :nginx_prefix, :domain, :cert_path, :template => 'benho
     sudo "mkdir -p #{cert_path}"
   }
   meet {
-    names.each {|name| sudo "cp '#{cert_path / name}' #{cert_path.to_s.end_with('/')}" }
+    names.each {|name| sudo "cp '#{cert_source / name}' #{cert_path.to_s.end_with('/')}" }
   }
 end
