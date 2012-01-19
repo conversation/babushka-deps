1bd0230a0398d3ffc5f544fea89f839cee8bd894
Ben Hoskings <ben@hoskings.net>
2012-01-19 13:41:27 +1100

    Add missing 'dw.theconversation.edu.au packages' dep.

--- dw.rb
+++ dw.rb
@@ -5,6 +5,12 @@ dep 'dw.theconversation.edu.au provisioned', :env, :app_root do
   ]
 end
 
+dep 'dw.theconversation.edu.au packages' do
+  requires [
+    'dw.theconversation.edu.au dev packages'
+  ]
+end
+
 dep 'dw.theconversation.edu.au dev packages' do
   requires [
     'socat.managed' # for DB tunnelling
