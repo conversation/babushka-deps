590e8cdc66500defd8e70e9cd3320bee1648d420
Ben Hoskings <ben@hoskings.net>
2012-02-08 12:36:00 +1100

    Rename domain to host_name when it's for 'system', and user_name when it's for 'user auth setup'.

--- base.rb
+++ base.rb
@@ -1,24 +1,24 @@
-dep 'system provisioned', :domain, :app_user, :password, :key do
+dep 'system provisioned', :host_name, :app_user, :password, :key do
   requires [
-    'base system provisioned'.with(domain, password, key),
+    'base system provisioned'.with(host_name, password, key),
     'benhoskings:running.nginx',
     "#{app_user} packages",
     'users setup'.with(app_user, password, key)
   ]
 end
 
-dep 'base system provisioned', :domain, :password, :key do
+dep 'base system provisioned', :host_name, :password, :key do
   requires [
     'benhoskings:user setup'.with(key: key),
-    'benhoskings:system'.with(host_name: domain),
+    'benhoskings:system'.with(host_name: host_name),
     'benhoskings:lamp stack removed',
     'benhoskings:postfix removed',
     'benhoskings:postgres.managed'
   ]
 end
 
-dep 'users setup', :domain, :password, :key do
-  requires 'benhoskings:user auth setup'.with(domain, password, key)
-  requires 'benhoskings:user auth setup'.with("mobwrite.#{domain}", password, key)
-  requires 'benhoskings:user auth setup'.with("chat.#{domain}", password, key)
+dep 'users setup', :username, :password, :key do
+  requires 'benhoskings:user auth setup'.with(username, password, key)
+  requires 'benhoskings:user auth setup'.with("mobwrite.#{username}", password, key)
+  requires 'benhoskings:user auth setup'.with("chat.#{username}", password, key)
 end
