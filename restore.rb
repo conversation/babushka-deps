2abc5043d364f85e28c6e1fce635201f3c925407
Ben Hoskings <ben@hoskings.net>
2012-02-16 18:54:23 +1100

    Temp

--- restore.rb
+++ restore.rb
@@ -1,6 +1,5 @@
-dep 'db recovered', :data_dir, :backup_host, :backup_name do
-
-  requires 'db recovery started'.with(data_dir, backup_host, backup_name)
+dep 'pg recovered', :data_dir, :backup_host, :backup_name do
+  requires 'pg recovery started'.with(data_dir, backup_host, backup_name)
   met? {
     loop {
       log "The data dir measures #{shell("du -s -m '#{data_dir}'").scan(/^\d+/).first}MB."
@@ -9,34 +8,36 @@ dep 'db recovered', :data_dir, :backup_host, :backup_name do
   }
 end
 
-dep 'db recovery started', :data_dir, :backup_host, :backup_name do
-  requires 'backup data in place'.with(data_dir, backup_host, backup_name)
+dep 'pg recovery started', :data_dir, :backup_host, :backup_name do
+  # requires 'pg backup in place'.with(data_dir, backup_host, backup_name)
+  met? {
+    (data_dir / 'postmaster.pid').exists?
+  }
   meet {
-    render_erb 'postgres/recovery.conf.erb', :to => (data_dir / 'recovery.conf')
-    shell("pg_ctl restart -D #{data_dir}")
+    shell('pg_ctl start -D /usr/local/var/postgres -l /usr/local/var/postgres/server.log')
   }
 end
 
-dep 'backup data in place', :data_dir, :backup_host, :backup_name do
-  requires_when_unmet 'empty local cluster'.with(data_dir)
-  met? {
-    
-  }
-  meet {
+dep 'pg backup in place', :data_dir, :backup_host, :backup_name, :template => 'task' do
+  requires 'pg shut down'.with(data_dir)
+  def src
+    "postgres@#{backup_host}:~/backups/#{backup_name}/"
+  end
+  def dest
+    "#{data_dir.to_s.chomp('/')}/"
+  end
+  run {
     log_block "rsyncing the #{backup_name} backup from #{backup_host} to #{data_dir}" do
-      shell "rsync -taP --delete 'postgres@#{backup_host}:~/backups/#{backup_name}/' #{data_dir.to_s.chomp('/')}/", :progress => /to-check=\d+\/\d+/
+      shell "rsync -taP --delete --exclude=postmaster.{opts,pid} '#{src}' '#{dest}'", :progress => /to-check=\d+\/\d+/
     end
   }
 end
 
-dep 'empty local cluster', :data_dir do
+dep 'pg shut down', :data_dir do
   met? {
-    data_dir.p.empty?
+    !(data_dir / 'postmaster.pid').exists?
   }
   meet {
-    local_dbs = shell("psql -l -t | sed -e 's/|.*$//'").split(/\n+/) - %w[postgres template0 template1]
-    confirm "The #{data_dir} cluster contains #{local_dbs.empty? ? 'no DBs' : local_dbs.to_list}. OK to delete?" do
-      data_dir.p.rm.mkdir
-    end
+    shell "pg_ctl stop --pgdata '#{data_dir}'"
   }
 end
