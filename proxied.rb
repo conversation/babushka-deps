c8ae75ebf724b0ebb51b2d951010b9554e0331d9
Ben Hoskings <ben@hoskings.net>
2012-02-21 14:22:52 +1100

    Fix 'proxied app', removing a method copy-pasted from dollhouse.

--- proxied.rb
+++ proxied.rb
@@ -1,22 +1,9 @@
-dep 'proxied app', :env, :app_name, :port do
+dep 'proxied app', :env, :app_name, :domain, :port do
   requires "#{app_name}".with(env, port)
   requires 'benhoskings:vhost enabled.nginx', {
-    domain: "#{name}.#{server_name}",
+    domain: domain,
     type: 'proxy',
     host: 'localhost',
     enable_ssl: 'no',
-  }.merge(opts)
-end
-
-def proxied_app name, domain, opts
-  creating_user "#{name}.#{domain}" do |username|
-    babushka "conversation:#{name}"
-
-    babushka 'benhoskings:vhost enabled.nginx', {
-      domain: "#{name}.#{server_name}",
-      type: 'proxy',
-      host: 'localhost',
-      enable_ssl: 'no',
-    }.merge(opts)
-  end
+  }
 end
