4905426603daf3e333e6a87a7f575379d02ecfa0
Gareth Townsend <gareth.townsend@me.com>
2012-02-21 14:48:57 +1100

    use .with instead of passing a hash

--- proxied.rb
+++ proxied.rb
@@ -1,9 +1,9 @@
 dep 'proxied app', :env, :app_name, :domain, :port do
   requires "#{app_name}".with(env)
-  requires 'benhoskings:vhost enabled.nginx', {
+  requires 'benhoskings:vhost enabled.nginx'.with(
     domain: domain,
     type: 'proxy',
     host: 'localhost',
     enable_ssl: 'no',
-  }
+  )
 end
