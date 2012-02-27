bab6047154aaddd79927b4731804b1c90c8275ff
Ben Hoskings <ben@hoskings.net>
2012-02-27 17:49:19 +1100

    Use 'git remote set-url' when the remote already exists.

--- git.rb
+++ git.rb
@@ -2,12 +2,15 @@ dep 'git remote', :remote_name, :user, :host do
   def remote_spec
     "#{user}@#{host}:~/current"
   end
+  def remote_command
+    shell('git remote').include?(remote_name.to_s) ? 'set-url' : 'add'
+  end
   met? {
     shell("git remote -v").split("\n").grep(
       /^#{Regexp.escape(remote_name)}\s+#{Regexp.escape(remote_spec)}\s+\(push\)$/
     ).any?
   }
   meet {
-    shell "git remote add #{remote_name} '#{remote_spec}'"
+    shell "git remote #{remote_command} #{remote_name} '#{remote_spec}'"
   }
 end
