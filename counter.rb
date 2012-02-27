20216002373866822773ed5dca95f8ac832c5564
Ben Hoskings <ben@hoskings.net>
2012-02-27 15:01:19 +1100

    Replace 'counter.theconversation.edu.au app' with a proper version.

--- counter.rb
+++ counter.rb
@@ -1,9 +1,21 @@
 dep 'counter.theconversation.edu.au system', :host_name, :app_user, :key
 
-dep 'counter.theconversation.edu.au app', :env, :app_root do
+dep 'counter.theconversation.edu.au app', :env, :domain, :app_user, :key, :app_root do
   requires [
-    'cronjobs'.with(env),
-    'geoip database'.with(app_root: app_root)
+    'benhoskings:user setup'.with(key: key),
+    'geoip database'.with(app_root: app_root),
+    'ssl certificate'.with(env, domain),
+
+    'benhoskings:rails app'.with(
+      env: env,
+      domain: domain,
+      username: app_user,
+      enable_https: 'no',
+      data_required: 'yes'
+    ),
+
+    # For the dw.theconversation.edu.au -> backup.tc-dev.net psql/ssh connection.
+    'read-only db permissions'.with("tc_#{env}", 'dw.theconversation.edu.au', 'content')
   ]
 end
 
