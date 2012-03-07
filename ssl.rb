a0b8e3f8d6af30d269089c8d468736935eafb81f
Ben Hoskings <ben@hoskings.net>
2012-03-07 17:42:24 +1100

    Restart nginx after putting the ssl cert in place.

--- ssl.rb
+++ ssl.rb
@@ -23,5 +23,6 @@ dep 'ssl cert in place', :nginx_prefix, :domain, :cert_name, :cert_source, :temp
     sudo "mkdir -p #{cert_path}"
     %w[key crt].all? {|ext| sudo "cp '#{cert_source / cert_name}.#{ext}' '#{cert_path / domain}.#{ext}'" }
     sudo "chmod 600 '#{cert_path / domain}'.*"
+    restart_nginx
   }
 end
