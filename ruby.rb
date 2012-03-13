92dde747d56c4621afc7327d8ec155944466b7e7
Michael Morris <mtcmorris@gmail.com>
2012-03-13 12:00:39 +1100

    Accept any ruby of the right version, not just the packaged one.

--- ruby.rb
+++ ruby.rb
@@ -5,4 +5,8 @@ dep 'ruby 1.9.managed', :version, :patchlevel do
   patchlevel.default!('p0')
   installs 'ruby1.9.1-full'
   provides "ruby == #{version}#{patchlevel}", 'gem', 'irb'
+
+  met? {
+    in_path? 'ruby 1.9.3p0'
+  }
 end
