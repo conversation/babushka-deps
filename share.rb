f1170edcb621e9f6db93776abf4ef4d1998f5b39
Michael Morris <mtcmorris@gmail.com>
2012-03-13 12:10:56 +1100

    Add sharejs provisioning

---
+++ share.rb
@@ -0,0 +1,48 @@
+dep 'sharejs.theconversation.edu.au system', :app_user, :key do
+  requires [
+
+    'benhoskings:user setup for provisioning'.with("mobwrite.#{app_user}", key),
+  ]
+end
+
+dep 'sharejs.theconversation.edu.au app', :env, :domain, :app_user, :app_root, :key do
+  requires [
+    'benhoskings:user setup'.with(:key => key),
+    'ssl certificate'.with(env, domain, 'theconversation.edu.au'),
+
+    "conversation:sharejs".with(:username => domain, :env => env, :domain => server_name),
+
+    "benhoskings:vhost enabled.nginx".with(
+      :type => 'proxy',
+      :domain => domain,
+      :proxy_host => 'localhost',
+      :proxy_port => 9000,
+      :enable_https => 'yes'
+    )
+  ]
+end
+
+dep 'sharejs.theconversation.edu.au packages' do
+  requires [
+    'benhoskings:running.nginx',
+    'supervisor.managed',
+    'theconversation.edu.au common packages'
+  ]
+end
+
+dep 'sharejs.theconversation.edu.au dev' do
+  requires [
+    'sharejs.theconversation.edu.au common packages',
+    'phantomjs', # for js testing
+    'geoip database'.with(:app_root => '.')
+  ]
+end
+
+dep 'sharejs.theconversation.edu.au common packages' do
+  requires [
+    'bundler.gem',
+    'postgres.managed',
+    "npm",
+    "conversation:coffeescript.src"
+  ]
+end
