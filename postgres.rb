8ceab0ea9a9ee1f837e47a4c5a471cd83887a8de
Ben Hoskings <ben@hoskings.net>
2012-03-20 18:08:36 +1100

    Move non-sharejs-specific DB deps to postgres.rb.

--- postgres.rb
+++ postgres.rb
@@ -1,9 +1,51 @@
+dep 'read-only schema access', :username, :owner_name, :db_name, :schema_name do
+  requires 'schema exists'.with(owner_name, db_name, schema_name)
+  met? {
+    raw_shell("psql #{db_name} -t -c '\\dn'", :as => 'postgres').stdout.val_for(schema_name) == "| #{username}"
+  }
+  meet {
+    sudo %Q{psql #{db_name} -c 'GRANT USAGE ON SCHEMA "#{schema_name}" TO "#{username}"'}, :as => 'postgres'
+  }
+end
+
 dep 'read-only db access', :db_name, :schema, :username, :check_table do
   schema.default!('public')
   check_table.default!('users')
   requires 'benhoskings:postgres access'.with(username)
   met? { shell? "psql #{db_name} -c 'SELECT id FROM #{check_table} LIMIT 1'", :sudo => username }
   meet { sudo %Q{psql #{db_name} -c 'GRANT SELECT ON ALL TABLES IN SCHEMA "#{schema}" TO "#{username}"'}, :as => 'postgres' }
+end
+
+dep 'table exists', :username, :db_name, :table_name, :table_schema do
+  if table_name['.']
+    requires 'schema exists'.with(username, db_name, table_name.to_s.split('.', 2).first)
+  end
+  met? {
+    shell? "psql #{db_name} -t -c '\\d #{table_name}'", :as => 'postgres'
+  }
+  meet {
+    sudo %Q{psql #{db_name} -c 'CREATE TABLE #{table_name} (#{table_schema});}, :as => 'postgres'
+  }
+end
+
+dep 'schema exists', :username, :db_name, :schema_name do
+  requires 'benhoskings:postgres access'.with(username)
+  met? {
+    raw_shell("psql #{db_name} -t -c '\\dn'", :as => 'postgres').stdout.val_for(schema_name)
+  }
+  meet {
+    sudo %Q{psql #{db_name} -c 'CREATE SCHEMA "#{schema_name}" AUTHORIZATION "#{username}"'}, :as => 'postgres'
+  }
+end
+
+dep 'schema ownership', :username, :db_name, :schema_name do
+  requires 'schema exists'.with(username, db_name, schema_name)
+  met? {
+    raw_shell("psql #{db_name} -t -c '\\dn'", :as => 'postgres').stdout.val_for(schema_name) == "| #{username}"
+  }
+  meet {
+    sudo %Q{psql #{db_name} -c 'ALTER SCHEMA "#{schema_name}" OWNER TO "#{username}"'}, :as => 'postgres'
+  }
 end
 
 dep 'postgres extension', :username, :db_name, :extension do
