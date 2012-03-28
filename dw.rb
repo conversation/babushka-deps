6b80230e2558fc846fe66444d791e5be9e9ff07a
Ben Hoskings <ben@hoskings.net>
2012-03-28 16:12:24 +1100

    Correct the 'dw.theconversation.edu.au app' params.

--- dw.rb
+++ dw.rb
@@ -1,6 +1,6 @@
 dep 'dw.theconversation.edu.au system', :app_user, :key
 
-dep 'dw.theconversation.edu.au app', :env, :app_root do
+dep 'dw.theconversation.edu.au app', :env, :domain, :app_user, :app_root, :key do
   requires [
     'cronjobs'.with(env),
     'minutely.cronjob'.with(env) # Also hook in per-minute cron tasks on the DW.
