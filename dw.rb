cfea624bad3d5ff68925d197df271a14fade53cb
Ben Hoskings <ben@hoskings.net>
2012-03-28 16:51:35 +1100

    Provision the dw app and db.

--- dw.rb
+++ dw.rb
@@ -3,7 +3,22 @@ dep 'dw.theconversation.edu.au system', :app_user, :key
 dep 'dw.theconversation.edu.au app', :env, :domain, :app_user, :app_root, :key do
   requires [
     'cronjobs'.with(env),
-    'minutely.cronjob'.with(env) # Also hook in per-minute cron tasks on the DW.
+    'minutely.cronjob'.with(env), # Also hook in per-minute cron tasks on the DW.
+
+    'benhoskings:rack app'.with(
+      :env => env,
+      :domain => domain,
+      :username => app_user,
+      :enable_https => 'yes'
+    ),
+
+    'benhoskings:db'.with(
+      :username => app_user,
+      :root => app_root,
+      :env => env,
+      :data_required => 'no',
+      :require_db_deps => 'yes'
+    )
   ]
 end
 
