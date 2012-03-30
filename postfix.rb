1c424faf8ab3a7b5b152cc3e6229811ac9142a3c
James Healy <jimmy@deefa.com>
2012-03-30 13:44:44 +1100

    first cut at some deps for configuring postfix

---
+++ postfix.rb
@@ -0,0 +1,44 @@
+def postfix_conf
+  "/etc/postfix/main.cf"
+end
+
+def restart_postfix
+  if postfix_running?
+    log_shell "restarting postfix", "invoke.rc-d postfix restart", :sudo => true
+  end
+end
+
+def postfix_running?
+  shell? "netstat -an | grep -E '^tcp.*[.:]25 +.*LISTEN'"
+end
+
+dep 'running.postfix' do
+
+  requires 'configured.postfix'
+
+  met? {
+    postfix_running?.tap {|result|
+      log "There is #{result ? 'something' : 'nothing'} listening on port 25."
+    }
+  }
+
+  meet :on => :linux do
+    sudo '/etc/init.d/postfix start'
+  end
+  meet :on => :osx do
+    log_error "launchctl should have already started postfix. Check /var/log/system.log for errors."
+  end
+end
+
+dep 'configured.postfix' do
+
+  requires 'postfix.managed'
+  met? {
+    Babushka::Renderable.new(postfix_conf).from?(dependency.load_path.parent / "postfix/main.cf.erb")
+  }
+  meet {
+    render_erb 'postfix/main.cf.erb', :to => postfix_conf, :sudo => true
+  }
+
+  after { restart_postfix }
+end
