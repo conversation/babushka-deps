0ee9a75697a3a49c9ec2456862be5f7c52e35f00
Ben Hoskings <ben@hoskings.net>
2012-04-26 10:45:09 +1000

    Accept a newer ruby than the one specified.

--- ruby.rb
+++ ruby.rb
@@ -4,5 +4,5 @@ dep 'ruby 1.9.managed', :version, :patchlevel do
   version.default!('1.9.3')
   patchlevel.default!('p0')
   installs 'ruby1.9.1-full'
-  provides "ruby == #{version}#{patchlevel}", 'gem', 'irb'
+  provides "ruby >= #{version}#{patchlevel}", 'gem', 'irb'
 end
