53b6eedae0580a01656978c2060bd8bd9408b0f9
Ben Hoskings <ben@hoskings.net>
2012-04-26 10:44:31 +1000

    Remove custom met? block in 'ruby' -- #provides is already there.

--- ruby.rb
+++ ruby.rb
@@ -5,8 +5,4 @@ dep 'ruby 1.9.managed', :version, :patchlevel do
   patchlevel.default!('p0')
   installs 'ruby1.9.1-full'
   provides "ruby == #{version}#{patchlevel}", 'gem', 'irb'
-
-  met? {
-    in_path? 'ruby 1.9.3p0'
-  }
 end
