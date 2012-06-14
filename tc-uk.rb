ecde18b0325ed2162deb5755d303c509e622ea1b
Gus Gollings <gus.gollings@gmail.com>
2012-06-14 10:35:09 +1000

    Require 'vhost enabled' from the right place.

--- tc-uk.rb
+++ tc-uk.rb
@@ -4,7 +4,7 @@ end
 dep 'theconversation.org.uk app', :env, :domain, :app_user, :app_root, :key do
   requires [
     # Replace the default config with our own.
-    'vhost enabled'.with(
+    'benhoskings:vhost enabled'.with(
       :app_name => 'tc-uk',
       :domain => domain,
       :path => app_root,
