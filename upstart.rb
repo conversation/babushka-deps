0b1acc1c3f52b22c55ca162e9fe3821b1ecc644c
Ben Hoskings <ben@hoskings.net>
2012-08-14 16:28:05 +1000

    Oops, fix 'setuid' references in the upstart template & config.

--- upstart.rb
+++ upstart.rb
@@ -7,7 +7,7 @@ meta :upstart do
   accepts_value_for :start_delay, 5
   template {
     def conf_name
-      "#{user}_#{basename.gsub(' ', '_')}"
+      "#{setuid}_#{basename.gsub(' ', '_')}"
     end
     def conf_dest
       "/etc/init/#{conf_name}.conf"
