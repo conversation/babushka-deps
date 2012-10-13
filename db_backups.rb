021829f2a74d7f758d73701871bcb614cc633201
Ben Hoskings <ben@hoskings.net>
2012-10-13 16:04:08 +1100

    Ratchet down psql backups with `ionice`, so they don't peg the server.

--- db_backups.rb
+++ db_backups.rb
@@ -9,7 +9,7 @@ dep 'db backup exists', :db_name, :backup_path do
 
   met? { backup_path.p.exists? }
   before { backup_prefix.mkdir }
-  meet { log_shell "Creating #{backup_path} from #{db_name}", "pg_dump #{db_name} | gzip -9 - > '#{backup_path}'" }
+  meet { log_shell "Creating #{backup_path} from #{db_name}", "pg_dump #{db_name} | ionice -c 3 gzip -9 - > '#{backup_path}'" }
   after { log_shell "Removing old sqldumps", %Q{ls -t -1 #{backup_prefix} | tail -n+3 | while read f; do rm "#{backup_prefix}/$f"; done} }
 end
 
