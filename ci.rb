6fe9edb960f80a6103b1073aa59150b673a1a7ba
Ben Hoskings <ben@hoskings.net>
2012-10-18 18:09:31 +1100

    Split keys, locale, and ruby out into a separate 'ci prepared' dep.

--- ci.rb
+++ ci.rb
@@ -1,11 +1,16 @@
-dep 'ci provisioned', :app_user, :ssh_key do
+dep 'ci prepared', :app_user, :ssh_key do
   requires [
     'benhoskings:passwordless ssh logins'.with(:username => 'root', :key => ssh_key),
-    'benhoskings:passwordless ssh logins'.with(:username => 'vagrant', :key => ssh_key),
+    'benhoskings:passwordless ssh logins'.with(:username => app_user, :key => ssh_key),
 
     'benhoskings:set.locale'.with(:locale_name => 'en_AU'),
     'benhoskings:ruby.src'.with(:version => '1.9.3', :patchlevel => 'p194'),
+  ]
+end
 
+dep 'ci provisioned', :app_user, :ssh_key do
+  requires [
+    'ci prepared'.with(app_user, ssh_key),
     'benhoskings:utc',
     'conversation:localhost hosts entry',
     'conversation:apt sources',
@@ -18,7 +23,10 @@ dep 'ci provisioned', :app_user, :ssh_key do
 end
 
 dep 'ci packages' do
-  requires 'openjdk-6-jdk'
+  requires [
+    'openjdk-6-jdk',
+    'selenium runtime'
+  ]
 end
 
 dep 'openjdk-6-jdk', :template => 'bin' do
@@ -34,4 +42,17 @@ dep 'jenkins target', :path, :app_user do
     path.p.mkdir
     shell "chown -R #{app_user} #{path}"
   }
+end
+
+dep 'selenium runtime', :template => 'lib' do
+  installs [
+    'xvfb',
+    'xserver-xorg-core',
+    'xfonts-100dpi',
+    'xfonts-75dpi',
+    'xfonts-scalable',
+    'xfonts-cyrillic',
+    'firefox',
+    'libqt4-dev'
+  ]
 end
