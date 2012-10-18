512b55f7edada980f2e8107f86c5a7ec557a9c13
Ben Hoskings <ben@hoskings.net>
2012-10-18 19:14:33 +1100

    Disable strict host key checking in the build agents.

--- ci.rb
+++ ci.rb
@@ -14,6 +14,7 @@ dep 'ci provisioned', :app_user, :public_key, :private_key do
     'ci prepared'.with(app_user, public_key, private_key),
     'benhoskings:utc',
     'conversation:localhost hosts entry',
+    'benhoskings:lax host key checking',
     'conversation:apt sources',
     'conversation:theconversation.edu.au common packages',
     'conversation:counter.theconversation.edu.au common packages',
