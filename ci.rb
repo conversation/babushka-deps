fe1cad373908618576e77c08b12c299ae4cc36c1
Ben Hoskings <ben@hoskings.net>
2012-10-18 15:50:09 +1100

    Add 'ci provisioned', ported from the Vagrantfile.

--- ci.rb
+++ ci.rb
@@ -1,3 +1,21 @@
+dep 'ci provisioned', :app_user, :ssh_key do
+  requires [
+    'benhoskings:passwordless ssh logins'.with(:username => 'root', :key => ssh_key),
+    'benhoskings:passwordless ssh logins'.with(:username => 'vagrant', :key => ssh_key),
+
+    'benhoskings:set.locale'.with(:locale_name => 'en_AU'),
+    'benhoskings:ruby.src'.with(:version => '1.9.3', :patchlevel => 'p194'),
+
+    'benhoskings:utc',
+    'conversation:localhost hosts entry',
+    'conversation:apt sources',
+    'conversation:theconversation.edu.au common packages',
+    'conversation:counter.theconversation.edu.au common packages',
+    'conversation:ci packages',
+    'benhoskings:postgres access'
+  ]
+end
+
 dep 'ci packages' do
   requires 'openjdk-6-jdk'
 end
