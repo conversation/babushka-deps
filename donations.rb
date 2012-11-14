34291b3ff0fdf84edf8d865840e3470267c5a87f
Ben Hoskings <ben@hoskings.net>
2012-11-14 14:42:46 +1100

    A draft of the provisioning deps for donations.

---
+++ donations.rb
@@ -0,0 +1,44 @@
+dep 'donate.theconversation.edu.au system', :app_user, :key
+
+dep 'donate.theconversation.edu.au app', :env, :domain, :app_user, :app_root, :key do
+  def db_name
+    YAML.load_file(app_root / 'config/database.yml')[env.to_s]['database']
+  end
+
+  requires [
+    'delayed job'.with(env, app_user),
+    'ssl cert in place'.with(:domain => domain, :cert_name => 'donate.theconversation.edu.au'),
+    'rails app'.with(
+      :app_name => 'donate',
+      :env => env,
+      :listen_host => host,
+      :domain => domain,
+      :username => app_user,
+      :path => app_root,
+      :data_required => 'yes'
+    )
+  ]
+end
+
+dep 'donate.theconversation.edu.au packages' do
+  requires [
+    'postgres'.with('9.2'),
+    'curl.lib',
+    'running.nginx',
+    'donate.theconversation.edu.au common packages'
+  ]
+end
+
+dep 'donate.theconversation.edu.au dev' do
+  requires 'donate.theconversation.edu.au common packages'
+end
+
+dep 'donate.theconversation.edu.au common packages' do
+  requires [
+    'bundler.gem',
+    'postgres.bin'.with('9.2'),
+    'libxml.lib', # for nokogiri
+    'libxslt.lib', # for nokogiri
+    'coffeescript.src' # for barista
+  ]
+end
