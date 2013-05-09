94fdd8f952142977e496fd0449fc066a146acc53
Ben Hoskings <ben@hoskings.net>
2013-05-09 16:11:35 +1000

    Merge 'webapp' into 'rack app'.

--- rack.rb
+++ rack.rb
@@ -1,18 +1,13 @@
 dep 'rack app', :app_name, :env, :domain, :username, :path, :listen_host, :listen_port, :enable_https, :proxy_host, :proxy_port, :nginx_prefix do
-  username.default!(shell('whoami'))
+  username.default!(domain)
   path.default('~/current')
   env.default(ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'production')
 
-  requires 'webapp'.with(app_name, env, domain, username, path, listen_host, listen_port, enable_https, proxy_host, proxy_port, nginx_prefix)
-  requires 'app bundled'.with(path, env)
-  requires 'rack.logrotate'.with(username)
-end
-
-dep 'webapp', :app_name, :env, :domain, :username, :path, :listen_host, :listen_port, :enable_https, :proxy_host, :proxy_port, :nginx_prefix do
-  username.default!(domain)
   requires [
     'user exists'.with(username, '/srv/http'),
+    'app bundled'.with(path, env),
     'vhost enabled.nginx'.with(app_name, env, domain, path, listen_host, listen_port, enable_https, proxy_host, proxy_port, nginx_prefix),
+    'rack.logrotate'.with(username),
     'running.nginx',
     'unicorn.upstart'.with(env, username)
   ]
