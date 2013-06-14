0343c9993835565ab200dac74bb7f6264ef3f093
James Healy <james@yob.id.au>
2013-06-14 16:47:43 +1000

    replace the contents of /etc/cron.hourly/ntpdate
    * don't append

--- time.rb
+++ time.rb
@@ -2,7 +2,7 @@ dep 'time is syncronised' do
   requires 'ntpdate.bin'
   met? { "/etc/cron.hourly/ntpdate".p.exists? }
   meet {
-    "/etc/cron.hourly/ntpdate".p.append("#!/bin/sh\n# -B - Always skew time\nntpdate -B pool.ntp.org")
+    "/etc/cron.hourly/ntpdate".p.write("#!/bin/sh\n# -B - Always skew time\nntpdate -B pool.ntp.org")
     shell "chmod +x /etc/cron.hourly/ntpdate"
   }
 end
