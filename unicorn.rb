1ee85c11f3a99dd7f9276e3cce32e16569bae25c
James Healy <james@yob.id.au>
2013-11-26 14:17:00 +1100

    unicorn upstart dep should also check if the upstart file needs to be rendered
    * just checking if the unicorns are running isn't enough. They might be
      running but without an upstart config that will start them on boot

--- unicorn.rb
+++ unicorn.rb
@@ -2,25 +2,40 @@ dep 'unicorn.upstart', :env, :user do
   def app_root
     "/srv/http/#{user}/current"
   end
+
+  def config_current?
+    if Babushka::Renderable.new(conf_dest).from?(dependency.load_path.parent / 'upstart/service.conf.erb')
+      true
+    else
+      log "upstart config needs updating"
+      false
+    end
+  end
+
+  def running?
+    running_count = shell('lsof -U').split("\n").grep(/#{Regexp.escape(app_root / 'tmp/sockets/unicorn.socket')}$/).count
+    (running_count >= 3).tap {|result| # 1 master + 2 workers
+      if result
+        log_ok "This app has #{running_count} unicorn#{'s' unless running_count == 1} running (1 master + #{running_count - 1} workers)."
+      elsif running_count > 0
+        unmeetable! "This app is in an unexpected state: (1 master + #{running_count - 1} workers)."
+      else
+        log "This app has no unicorns running."
+      end
+    }
+  end
+
   task 'yes'
   command "bundle exec unicorn -D -E #{env} -c config/unicorn.rb"
   setuid user
   chdir app_root
+
   met? {
     if !(app_root / 'config/unicorn.rb').exists?
       log "Not starting any unicorns because there's no unicorn config."
       true
     else
-      running_count = shell('lsof -U').split("\n").grep(/#{Regexp.escape(app_root / 'tmp/sockets/unicorn.socket')}$/).count
-      (running_count >= 3).tap {|result| # 1 master + 2 workers
-        if result
-          log_ok "This app has #{running_count} unicorn#{'s' unless running_count == 1} running (1 master + #{running_count - 1} workers)."
-        elsif running_count > 0
-          unmeetable! "This app is in an unexpected state: (1 master + #{running_count - 1} workers)."
-        else
-          log "This app has no unicorns running."
-        end
-      }
+      config_current? && running?
     end
   }
 end
