9c9080088506f0d8e4e51f7780c71037a1c3be36
James Healy <james@yob.id.au>
2014-02-07 14:29:45 +1100

    add a dep for precompiling rails assets
    * We used to Ben's "assets precompiled" dep, but it's not rails4
      friendly

--- deploy.rb
+++ deploy.rb
@@ -1,3 +1,9 @@
+dep 'assets precompiled during deploy', :env, :deploying, :template => 'task' do
+  run {
+    shell "bundle exec rake assets:precompile RAILS_ENV=#{env}"
+  }
+end
+
 dep 'upload assets', :env, :deploying do
   met? {
     shell? "bundle exec rake tc:assets:upload_required RAILS_ENV=#{env}"
