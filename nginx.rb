dea39267bdea7a532c00a56d685877d999431a2a
James Healy <james@yob.id.au>
2014-03-19 09:51:22 +1100

    upgrade nginx to the latest stable version

--- nginx.rb
+++ nginx.rb
@@ -129,7 +129,7 @@ end
 
 dep 'nginx.src', :nginx_prefix, :version do
   nginx_prefix.default!("/opt/nginx")
-  version.default!('1.4.4')
+  version.default!('1.4.7')
 
   requires 'pcre.lib', 'ssl.lib', 'zlib.lib'
 
