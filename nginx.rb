ea1006383a6775cf038d0bde6e081ff13c91921a
James Healy <james@yob.id.au>
2014-10-08 21:57:45 +1100

    update nginx from 1.6.0 to 1.6.2
    * includes some security fixes

--- nginx.rb
+++ nginx.rb
@@ -135,7 +135,7 @@ end
 
 dep 'nginx.src', :nginx_prefix, :version do
   nginx_prefix.default!("/opt/nginx")
-  version.default!('1.6.0')
+  version.default!('1.6.2')
 
   requires 'pcre.lib', 'ssl.lib', 'zlib.lib'
 
