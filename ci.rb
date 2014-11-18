22db6bd73f99e484cc29384474d1c3de1a09c236
James Healy <james@yob.id.au>
2014-11-19 00:56:01 +1100

    upgrade CI to ruby 2.1.5

--- ci.rb
+++ ci.rb
@@ -1,7 +1,7 @@
 dep 'ci prepared' do
   requires [
     'set.locale'.with(:locale_name => 'en_AU'),
-    'ruby.src'.with(:version => '2.1.4', :patchlevel => 'p265'),
+    'ruby.src'.with(:version => '2.1.5', :patchlevel => 'p273'),
   ]
 end
 
