705121071fbdff1e613412b7ef763b33121ca031
James Healy <james@yob.id.au>
2015-03-02 16:53:44 +1100

    write a /etc/default/unbound config file during provisioning
    * turn off root hints and resolvconf integration

--- unbound.rb
+++ unbound.rb
@@ -13,10 +13,20 @@ dep 'unbound configured' do
     "/etc/unbound/unbound.conf"
   end
 
+  def renderable_default
+    "unbound/default"
+  end
+
+  def system_default
+    "/etc/default/unbound"
+  end
+
   met? {
-    Babushka::Renderable.new(system_conf).from?(dependency.load_path.parent / renderable_conf)
+    Babushka::Renderable.new(system_conf).from?(dependency.load_path.parent / renderable_conf) &&
+     Babushka::Renderable.new(system_default).from?(dependency.load_path.parent / renderable_default)
   }
   meet {
     render_erb renderable_conf, :to => system_conf, :sudo => true
+    render_erb renderable_default, :to => system_default, :sudo => true
   }
 end
