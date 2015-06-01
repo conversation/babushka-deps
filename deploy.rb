90a4078bb3281a661e262ea97ad89c0a03d2da3e
Ben Hoskings <ben@hoskings.net>
2015-06-01 11:53:02 +0900

    Use 'common:assets precompiled' during deploys.

--- deploy.rb
+++ deploy.rb
@@ -5,12 +5,6 @@ def on_standby?
   !psql_output.nil? && psql_output['on']
 end
 
-dep 'assets precompiled during deploy', :env, :deploying, :template => 'task' do
-  run {
-    shell "bundle exec rake assets:precompile RAILS_ENV=#{env}"
-  }
-end
-
 dep 'upload assets', :env, :deploying do
   met? {
     shell? "bundle exec rake tc:assets:upload_required RAILS_ENV=#{env}"
