d5c31c774e14ddfc364f7b67cd1f8886912c2ac1
Josh Bassett <josh.bassett@gmail.com>
2016-12-21 11:53:44 +1100

    Refactor puma upstart config
    The custom `puma upstart config` dep has been refactored to use the more general `upstart` template.

--- puma.rb
+++ puma.rb
@@ -1,19 +1,12 @@
-dep 'puma upstart config', :env, :user do
-  def app_root
-    "/srv/http/#{user}/current"
-  end
+dep 'puma.upstart', :env, :path, :username, :threads, :workers do
+  threads.default!('4')
+  workers.default!('1')
 
-  def template_path
-    dependency.load_path.parent / 'puma/puma.init.erb'
-  end
-
-  def service_name
-    "#{user}_puma"
-  end
-
-  def conf_dest
-    "/etc/init/#{service_name}.conf"
-  end
+  respawn 'yes'
+  command './bin/puma'
+  setuid username
+  chdir path.p.abs
+  environment "RACK_ENV=#{env}", "RAILS_ENV=#{env}", "PUMA_THREADS=#{threads}", "PUMA_WORKERS=#{workers}"
 
   def config_current?
     if Babushka::Renderable.new(conf_dest).from?(template_path)
@@ -25,7 +18,7 @@ dep 'puma upstart config', :env, :user do
   end
 
   def running?
-    running_count = shell('lsof -U').split("\n").grep(/#{Regexp.escape(app_root / 'tmp/sockets/puma.socket')}$/).count
+    running_count = shell('lsof -U').split("\n").grep(/#{Regexp.escape(path / 'tmp/sockets/puma.socket')}$/).count
     if running_count > 0
       log_ok "There are #{running_count} puma processes running."
     else
@@ -34,17 +27,12 @@ dep 'puma upstart config', :env, :user do
   end
 
   met? {
-    if !(app_root / 'config/puma.rb').exists?
+    if !(path / 'config/puma.rb').exists?
       log "Not starting any pumas because there's no puma config."
       true
     else
       config_current? && running?
     end
-  }
-  meet {
-    render_erb template_path, :to => conf_dest, :sudo => true
-    sudo "initctl start #{service_name}; true"
-    sleep 10
   }
 end
 
