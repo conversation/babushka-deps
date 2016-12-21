c6fa246b812d832b2268e4980a1fb901653f86e4
Josh Bassett <josh.bassett@gmail.com>
2016-12-21 11:56:17 +1100

    Specify threads in counter dep

--- counter.rb
+++ counter.rb
@@ -25,7 +25,8 @@ dep 'counter app', :env, :host, :domain, :app_user, :app_root, :key do
       :listen_host => host,
       :domain => domain,
       :username => app_user,
-      :path => app_root
+      :path => app_root,
+      :threads => 8
     )
   ]
 end
