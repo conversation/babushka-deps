459b619c213e6449a2a2c686988eafeffdeac299
James Healy <james@yob.id.au>
2016-12-27 13:39:35 +1100

    tc now requires these two postgresql extensions

--- tc.rb
+++ tc.rb
@@ -14,6 +14,8 @@ dep 'tc app', :env, :host, :domain, :app_user, :app_root, :key do
 
   requires [
     'postgres extension'.with(app_user, db_name, 'unaccent'),
+    'postgres extension'.with(app_user, db_name, 'pg_trgm'),
+    'postgres extension'.with(app_user, db_name, 'fuzzystrmatch'),
     'geoip database'.with(:app_root => app_root),
 
     'ssl cert in place'.with(:domain => domain, :env => env)
