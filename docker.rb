3e8405445684ee1844241bdf897939a08d463895
Josh Bassett <josh.bassett@gmail.com>
2017-05-22 14:17:56 +1000

    WIP

---
+++ docker.rb
@@ -0,0 +1,20 @@
+dep 'docker.bin', :version do
+  version.default!('17.03.1')
+
+  requires_when_unmet {
+    on :apt, 'keyed apt source'.with(
+      :uri => 'https://download.docker.com/linux/ubuntu',
+      :release => 'xenial',
+      :repo => 'stable',
+      :key_sig => '0EBFCD88',
+      :key_uri => 'https://download.docker.com/linux/ubuntu/gpg'
+    )
+  }
+
+  installs {
+    via :apt, "docker-ce"
+    via :brew, "docker"
+  }
+
+  provides "docker >= #{version}"
+end
