1ce0f6db1ff6456e2d433bbe4c2f5b05907daaf3
Josh Bassett <josh.bassett@gmail.com>
2017-05-22 14:17:53 +1000

    Fix postgres to use xenial source

--- postgres.rb
+++ postgres.rb
@@ -176,7 +176,7 @@ dep 'postgres.bin', :version do
   requires_when_unmet {
     on :apt, 'keyed apt source'.with(
       :uri => 'http://apt.postgresql.org/pub/repos/apt/',
-      :release => 'trusty-pgdg',
+      :release => 'xenial-pgdg',
       :repo => 'main',
       :key_sig => 'ACCC4CF8',
       :key_uri => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc'
