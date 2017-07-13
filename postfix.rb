cf75e63f6a92c149d6c7ecfe2cddf832ee15524c
Josh Bassett <josh.bassett@gmail.com>
2017-07-13 10:04:07 +1000

    Configure postfix to use mailgun for delivering mail

--- postfix.rb
+++ postfix.rb
@@ -3,21 +3,28 @@ meta :postfix do
     "/etc/postfix/main.cf"
   end
 
+  def sasl_passwd
+    "/etc/postfix/sasl_passwd"
+  end
+
+  def start_postfix
+    log_shell "Starting postfix...", "systemctl start postfix"
+  end
+
   def restart_postfix
     if postfix_running?
-      log_shell "restarting postfix", "/etc/init.d/postfix restart"
+      log_shell "Restarting postfix...", "systemctl restart postfix"
     end
   end
 
   def postfix_running?
-    shell? "netstat -an | grep -E '^tcp.*[.:]25 +.*LISTEN'"
+    shell? "systemctl is-active postfix"
   end
 end
 
 dep 'postfix.bin'
 
 dep 'running.postfix' do
-
   requires 'configured.postfix'
 
   met? {
@@ -27,14 +34,14 @@ dep 'running.postfix' do
   }
 
   meet :on => :linux do
-    shell '/etc/init.d/postfix start'
+    start_postfix
   end
   meet :on => :osx do
     log_error "launchctl should have already started postfix. Check /var/log/system.log for errors."
   end
 end
 
-dep 'configured.postfix' do
+dep 'configured.postfix', :mailgun_password do
   def hostname
     shell('hostname -f').chomp
   end
@@ -42,9 +49,13 @@ dep 'configured.postfix' do
   requires 'postfix.bin'
   met? {
     Babushka::Renderable.new(postfix_conf).from?(dependency.load_path.parent / "postfix/main.cf.erb")
+    Babushka::Renderable.new(sasl_passwd).from?(dependency.load_path.parent / "postfix/sasl_passwd.erb")
   }
   meet {
-    render_erb 'postfix/main.cf.erb', :to => postfix_conf
+    render_erb 'postfix/main.cf.erb', to: postfix_conf
+    render_erb 'postfix/sasl_passwd.erb', to: sasl_passwd
+    shell 'chmod 600 /etc/postfix/sasl_passwd'
+    shell 'postmap /etc/postfix/sasl_passwd'
   }
 
   after { restart_postfix }
