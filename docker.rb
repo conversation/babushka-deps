b1e8a2f4cd92603ab0051a2378ef30eda8274451
Josh Bassett <josh.bassett@gmail.com>
2017-09-15 10:08:02 +1000

    Update docker to 17.06.2

--- docker.rb
+++ docker.rb
@@ -1,5 +1,5 @@
 dep 'docker.bin', :version do
-  version.default!('17.03.1-ce')
+  version.default!('17.06.2-ce')
 
   requires_when_unmet {
     on :apt, 'keyed apt source'.with(
@@ -20,12 +20,12 @@ dep 'docker.bin', :version do
 end
 
 dep 'docker-compose', :version do
-  version.default!('1.13.0')
+  version.default!('1.15.0')
   met? {
     in_path? "docker-compose >= #{version}"
   }
   meet {
-    shell "curl -L https://github.com/docker/compose/releases/download/1.13.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+    shell "curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
     shell "chmod a+x /usr/local/bin/docker-compose"
   }
 end
