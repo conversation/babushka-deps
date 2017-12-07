f69cd9764f82bbf812a71297b420004c60bd112d
Josh Bassett <josh.bassett@gmail.com>
2017-12-08 10:52:12 +1100

    Remove unused nginx logrotate config

--- logrotate.rb
+++ logrotate.rb
@@ -20,11 +20,6 @@ meta :logrotate do
   }
 end
 
-dep 'nginx.logrotate', :for => :linux do
-  renders "logrotate/nginx.conf"
-  as "nginx"
-end
-
 dep 'rack.logrotate', :username do
   renders "logrotate/rack.conf"
   as username
