bc4dc231e58ea386707a0e07e31b49ee29734d0c
Josh Bassett <josh.bassett@gmail.com>
2017-12-07 15:05:08 +1100

    Fix replication deps

--- replication.rb
+++ replication.rb
@@ -1,79 +1,52 @@
-dep 'postgres master', :version, :local_user, :key do
-  local_user.default!('standby')
+dep 'postgres master', :username, :password do
+  username.default!('standby')
 
-  requires [
-    'postgres access'.with(local_user, '-SDR --replication'),
-    'passwordless ssh logins'.with(local_user, key)
-  ]
+  # Create the standby user with replication privileges.
+  requires 'postgres access'.with(username: username, flags: '--replication')
 end
 
-dep 'postgres standby', :env, :version, :local_user, :local_port, :remote_user, :remote_host do
-  env.default(ENV['RACK_ENV'])
-  local_port.default!(5433)
-  remote_host.default({
-    'production' => 'theconversation.edu.au',
+dep 'postgres standby', :host, :env, :version, :username, :password do
+  host.default({
+    'production' => 'prod-master.tc-dev.net',
     'staging' => 'staging.tc-dev.net'
   }[env])
+  env.default(ENV['RACK_ENV'])
+  version.default!('10.1')
+  username.default!('standby')
 
-  requires [
-    'postgres socket tunnel.systemd'.with(local_user, local_port, remote_user, remote_host),
-    'postgres recovery config'.with(version, local_port, remote_user)
-  ]
+  requires 'postgres recovery config'.with(host: host, version: version, username: username, password: password)
 end
 
-dep 'postgres recovery config', :version, :local_port, :remote_user do
-  def minor_version
-    version.to_s.scan(/^\d\.\d/).first
-  end
+dep 'postgres recovery config', :host, :version, :username, :password do
   requires 'postgres.bin'.with(version)
+
   def psql cmd
     shell?("psql postgres -c 'SELECT 1'", :as => 'postgres') &&
     shell("psql postgres -t", :as => 'postgres', :input => cmd).strip
   end
+
+  def minor_version
+    Util.minor_version(version)
+  end
+
   met? {
     psql('SELECT pg_is_in_recovery()') == 't'
   }
+
   meet {
     render_erb "postgres/recovery.conf.erb", :to => "/var/lib/postgresql/#{minor_version}/main/recovery.conf"
     shell "chown postgres:postgres /var/lib/postgresql/#{minor_version}/main/recovery.conf"
-    log_shell "Restarting postgres...", "systemctl restart postgresql", sudo: true
+    Util.restart_service('postgresql')
   }
 end
 
-dep 'postgres socket tunnel.systemd', :local_user, :local_port, :remote_user, :remote_host do
-  local_user.default!('postgres')
-  remote_user.default!('standby')
-
-  requires 'socat.bin'
-
-  def service_name
-    "postgres_socket_tunnel"
-  end
-
-  def local_socket
-    "/var/run/postgresql/.s.PGSQL.#{local_port}"
-  end
-
-  def remote_socket
-    "/var/run/postgresql/.s.PGSQL.5432"
-  end
-
-  description "Postgres socket tunnel"
-  respawn 'yes'
-
-  command %Q(/usr/bin/socat UNIX-LISTEN:#{local_socket},fork EXEC:'ssh -C #{remote_user}@#{remote_host} "socat STDIO UNIX-CONNECT:#{remote_socket}"')
-
-  setuid local_user
-  chdir "/var/lib/postgresql"
-end
-
 dep 'postgres replication monitoring', :version, :test_user do
-
   requires 'postgres.bin'.with(version)
 
   met? {
     shell? 'psql postgres -c "SELECT replication_status()"', :as => test_user
   }
+
   meet {
     shell 'psql postgres', :as => 'postgres', :input => %q{
       BEGIN;
