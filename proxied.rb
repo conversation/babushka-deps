f73de335ceaa02830fe5894d0d1f74abed38e52b
Josh Bassett <josh.bassett@gmail.com>
2018-02-01 10:37:46 +1100

    Remove 'proxied app' dep

--- proxied.rb
+++
@@ -1,9 +0,0 @@
-dep "proxied app", :env, :app_name, :domain, :port do
-  requires "#{app_name}".with(env)
-  requires "vhost enabled.nginx".with(
-    domain: domain,
-    type: "proxy",
-    proxy_host: "localhost",
-    proxy_port: port
-  )
-end
